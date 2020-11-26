9fbf8354ce4e7bacf5cf910c7587074eb1491e5e
Kumar Nitesh <niteshlike@gmail.com>
2020-11-26 14:10:46 +0530

    Add files via upload

---
+++ File_Move_Wait.ps1
@@ -0,0 +1,23 @@
+﻿##########################################################################################
+$source1 = "D:\Temp1"
+$destination1 = "D:\Temp"
+$filecount1 = "2"
+$waitinterval1 = "20"
+
+##########################################################################################
+do {
+$1 = Get-ChildItem -Path $source1
+$2 = $1 | select -First $filecount1
+$2|%{
+$f = $_.fullname
+$f1 = $_.name
+write-host 'Moved File: ' $f1  -foregroundcolor white -backgroundcolor green
+Move-Item -Path $f -Destination $destination1
+}
+Write-host "Waiting for next iteration" -foregroundcolor white -backgroundcolor red
+Start-Sleep -Seconds $waitinterval1
+}
+while 
+($1.count -gt 0)
+
+##########################################################################################
